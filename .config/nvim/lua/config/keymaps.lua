f575b5c8bc9be1f443cc06145ed47d92da7cfe78
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-10-20 09:59:52 +1300

    feat(nvim): add keymap for horizontal window switching with `Ctrl` + `[` and `]`

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- For easing switching between Windows (i.e. moving between the active buffer and neo-tree)
+map("n", "<C-[>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-]>", "<C-w>l", { desc = "Go to right window", remap = true })
