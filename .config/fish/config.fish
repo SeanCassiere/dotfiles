f34a1f6b1ec8a87f7b5070bc279beba22f574844
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-10-20 15:25:57 +1300

    feat(fish): tmux environment variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 ## Disable the Fish Shell greeting
 set -U fish_greeting
 
+## Use Tmux integrations
+set ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX yes
+
 ## Navigation alias
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -22,7 +25,7 @@ end
 
 ## Ls command formatted showing hidden files
 function lsa
-    ls -lah $argv 
+    ls -lah $argv
 end
 
 ## mkdir and cd into the directory
@@ -34,8 +37,8 @@ end
 function copy
     set count (count $argv | tr -d \n)
     if test "$count" = 2; and test -d "$argv[1]"
-	set from (echo $argv[1] | trim-right /)
-	set to (echo $argv[2])
+        set from (echo $argv[1] | trim-right /)
+        set to (echo $argv[2])
         command cp -r $from $to
     else
         command cp $argv
@@ -57,44 +60,44 @@ end
 ## Default: https://github.com/oh-my-fish/theme-default
 ## Fishy-Drupal: https://github.com/oh-my-fish/theme-fishy-drupal
 function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+    echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
 function fish_prompt
-  set -l current_user (whoami)
-  set -l last_command_status $status
-  
-  set -l fish     "⋊>"
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l green (set_color -o green)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l normal (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
+    set -l current_user (whoami)
+    set -l last_command_status $status
 
-  set -l cwd (set_color $fish_color_cwd)(prompt_pwd)
+    set -l fish "⋊>"
+    set -l cyan (set_color -o cyan)
+    set -l yellow (set_color -o yellow)
+    set -l green (set_color -o green)
+    set -l red (set_color -o red)
+    set -l blue (set_color -o blue)
+    set -l normal (set_color normal)
+    set -l success_color (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
+    set -l error_color (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
 
-  if [ (_git_branch_name) ]
-    set -l git_branch $red(_git_branch_name)
-    set git_info "$blue git:($git_branch$blue)"
+    set -l cwd (set_color $fish_color_cwd)(prompt_pwd)
 
-    if [ (_is_git_dirty) ]
-      set -l dirty "$yellow ✗"
-      set git_dirty "$dirty"
+    if [ (_git_branch_name) ]
+        set -l git_branch $red(_git_branch_name)
+        set git_info "$blue git:($git_branch$blue)"
+
+        if [ (_is_git_dirty) ]
+            set -l dirty "$yellow ✗"
+            set git_dirty "$dirty"
+        end
     end
-  end
 
-  if test $last_command_status -eq 0
-    echo -n -s $current_user ' ' $success_color $fish $normal ' ' $cwd $git_info $git_dirty $normal ' '
-  else
-    echo -n -s $current_user ' ' $error_color $fish $normal ' ' $cwd $git_info $git_dirty $normal ' '
-  end
+    if test $last_command_status -eq 0
+        echo -n -s $current_user ' ' $success_color $fish $normal ' ' $cwd $git_info $git_dirty $normal ' '
+    else
+        echo -n -s $current_user ' ' $error_color $fish $normal ' ' $cwd $git_info $git_dirty $normal ' '
+    end
 
 end
 ## prompt end
@@ -106,3 +109,4 @@ fish_add_path /opt/homebrew/sbin
 ## https://riptutorial.com/node-js/example/17273/installing-with-node-version-manager-under-fish-shell-with-oh-my-fish-
 fish_add_path ~/.nvm
 fish_add_path /Users/seanc/Library/Android/sdk/platform-tools/
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
